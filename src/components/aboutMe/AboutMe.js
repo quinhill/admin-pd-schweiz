899881a1d17c7dcb42cd1b800a63d107c78fc51c
quinhill@icloud.com <quinhill@icloud.com>
2019-02-22 10:13:49 -0700

    Set up barebones of AboutMe component

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'react';
+
+class AboutMe extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: []
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default AboutMe;
